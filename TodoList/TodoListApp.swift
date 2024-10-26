b1ba89bd2488caee606a0b9435029b76d9dfd766
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2024-10-26 12:20:09 +0200

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Manyuchi, Carrington C on 2024/10/26.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
